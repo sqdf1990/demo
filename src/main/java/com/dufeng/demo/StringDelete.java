437033f87c8189b24f7420f5ba939847aa41729b
dufeng <dufeng@dufengs-MacBook-Pro.local>
2018-11-15 21:17:23 +0800

    增加了注释说明

--- src/main/java/com/dufeng/demo/StringDelete.java
+++ src/main/java/com/dufeng/demo/StringDelete.java
@@ -16,9 +16,9 @@ public class StringDelete {
         if (str == null || str.length() < targetStr.length()) {
             return str;
         }
-
+        //用来存放过滤后的字符的栈
         Stack<Character> stack = new Stack<Character>();
-
+        //如果有n个"mi"，这个循环会执行2n+常数个非m、i字符个数步骤，即时间复杂度为O(2n)
         for (char c : str.toCharArray()) {
             if (c == 'i' && !stack.isEmpty() && stack.peek() == 'm') {
                 stack.pop();
@@ -35,7 +35,11 @@ public class StringDelete {
     }
 
     public static void main(String[] args) {
+        //输出amb
         System.out.println(delete("ammimmiib"));
+        //输出ab
         System.out.println(delete("ammimiib"));
+        //输出abmm
+        System.out.println(delete("ammimiibmimm"));
     }
 }
